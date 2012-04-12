a4387a8dcf214e5e092eeb70fe9e88cb144d5a4c
Philip Cristiano <philipcristiano@gmail.com>
2012-04-12 18:35:51 -0400

    load monitor: Record each metric individually

--- monitor/monitors/load.py
+++ monitor/monitors/load.py
@@ -5,7 +5,18 @@ from monitor.monitors.base_monitor import BaseMonitor
 def monitor_load(record):
     "Monitors system load"
     loadavg = open('/proc/loadavg').readall()
-    record('load {0}'.format(loadavg))
+    load_values = loadavg.split()
 
+    data = {
+        '1_min': float(load_values[0]),
+        '5_min': float(load_values[1]),
+        '15_min': float(load_values[2]),
+        'runnable_tasks': float(load_values[3].split('/')[0]),
+        'total_tasks': float(load_values[3].split('/')[1]),
+        'last_pid': float(load_values[4]),
+    }
+
+    for name, value in data.items():
+        record(name, value)
 
 register('load', monitor_load)
--- tests/unit/monitors/test_load.py
+++ tests/unit/monitors/test_load.py
@@ -7,8 +7,26 @@ class TestLoad(object):
     def setup(self):
         self.record = MagicMock()
         self.open_mock = MagicMock()
+        self.loadavg = '1.44 1.39 0.72 1/151 1565'
+        self.line = self.open_mock.return_value.readall.return_value = self.loadavg
 
-    def test_records_load(self):
         with patch('__builtin__.open', self.open_mock) as open_mock:
             monitor_load(self.record)
-        self.open_mock.assert_called_with('/proc/loadavg')
+
+    def test_records_1min(self):
+        self.record.assert_any_call('1_min', 1.4399999999999999)
+
+    def test_records_5min(self):
+        self.record.assert_any_call('5_min', 1.3899999999999999)
+
+    def test_records_15min(self):
+        self.record.assert_any_call('15_min', 0.71999999999999997)
+
+    def test_records_runnable_tasks(self):
+        self.record.assert_any_call('runnable_tasks', 1.0)
+
+    def test_records_total_tasks(self):
+        self.record.assert_any_call('total_tasks', 151.0)
+
+    def test_records_last_pid(self):
+        self.record.assert_any_call('last_pid', 1565.0)
