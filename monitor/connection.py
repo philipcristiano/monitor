b28abcdbdd1bc48e759fb37214b6ae0d93581faa
Philip Cristiano <philipcristiano@gmail.com>
2012-04-15 09:26:14 -0400

    connection: Sleep for 1 second to prevent Puka crash

--- monitor/connection.py
+++ monitor/connection.py
@@ -1,8 +1,11 @@
+import time
+
 import puka
 
 def create_connection():
     """Creates an AMQP connection"""
     client = puka.Client("amqp://33.33.33.10/")
     promise = client.connect()
+    time.sleep(1)
     client.wait(promise)
     return client
